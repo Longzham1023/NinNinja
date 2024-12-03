ad851b6456fda3627e30d8c9331f7ba7c796dc50
Longzham1023 <trannamlong203@gmail.com>
2024-12-03 01:19:40 +0000

    Complete game

--- Assets/_Game/Scripts/MovingPlat.cs
+++ Assets/_Game/Scripts/MovingPlat.cs
@@ -25,4 +25,19 @@ public class MovingPlat : MonoBehaviour
             target = aPoint.position;
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.transform.SetParent(null);
+        }
+    }
 }
